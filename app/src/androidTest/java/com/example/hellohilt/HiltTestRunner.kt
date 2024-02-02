afe511ca6bed271feb64d5f39fd4bbbce67128b8
Ryo SUETSUGU <suetsugu.r@gmail.com>
2024-02-02 16:48:30 +0900

    instrumentation test

---
+++ app/src/androidTest/java/com/example/hellohilt/HiltTestRunner.kt
@@ -0,0 +1,15 @@
+package com.example.hellohilt
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class HiltTestRunner: AndroidJUnitRunner() {
+    override fun newApplication(
+        cl: ClassLoader,
+        appName: String,
+        context: Context) : Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.getName(), context)
+    }
+}
